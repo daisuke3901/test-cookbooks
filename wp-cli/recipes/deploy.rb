0198898c6c569585c1f0ad560e185c156e09aca6
EC2 Default User <ec2-user@ip-172-27-23-24.ap-northeast-1.compute.internal>
2020-02-25 07:53:26 +0000

    create wp-cli recipe

--- wp-cli/recipes/deploy.rb
+++ wp-cli/recipes/deploy.rb
@@ -1,17 +1,3 @@
-wpdir = "/usr/bin/"
-
-remote_file "#{wpdir}/wp" do
-  source "https://download-for-aws-training2.s3-ap-northeast-1.amazonaws.com/wp-cli-1.5.1.phar"
-  owner "root"
-  group "root"
-  mode 00755
-  checksum "0cc7a95e68a2ef02fc423614806c29a8e76e4ac8c9b3e67d6673635d6eaea871"
-end
------------- default.rb end-------------------
-
-vi wp-cli/recipes/deploy.rb
-
------------- deploy.rb start------------------
 require "net/http"
 require "uri"
 
